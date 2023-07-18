cd8c4c9b4a6dbb7bfb529d2ab663de3d1280d58c
jmblack15 <jm157653@gmail.com>
2023-07-17 23:15:56 -0500

    starting project

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+
+import {COLORS, icons, images, SIZES} from '../constants';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
